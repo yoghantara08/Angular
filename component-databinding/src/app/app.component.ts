de3c8819fbb53bfc0388732ced24c5b28fee24b8
Yoghantara <gyoghantara@gmail.com>
2022-07-10 16:08:43 +0800

    custom properties

--- component-databinding/src/app/app.component.ts
+++ component-databinding/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [
+    { type: "server", name: "Test server", content: "Just a test!" },
+    { type: "blueprint", name: "Test blueprint", content: "Just a test!" },
+  ];
 }
