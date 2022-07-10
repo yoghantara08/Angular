f173bba0ac7cc2725225bda6b12d20ddb8707232
Yoghantara <gyoghantara@gmail.com>
2022-07-10 18:52:47 +0800

    create and inject service

--- services/src/app/account/account.component.ts
+++ services/src/app/account/account.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { LoggingService } from "../logging.service";
 
 @Component({
   selector: "app-account",
   templateUrl: "./account.component.html",
   styleUrls: ["./account.component.css"],
+  providers: [LoggingService],
 })
 export class AccountComponent {
   @Input() account: { name: string; status: string };
@@ -13,8 +15,10 @@ export class AccountComponent {
     newStatus: string;
   }>();
 
+  constructor(private loggingService: LoggingService) {}
+
   onSetTo(status: string) {
     this.statusChanged.emit({ id: this.id, newStatus: status });
-    console.log("A server status changed, new status: " + status);
+    this.loggingService.logStatusChange(status);
   }
 }
